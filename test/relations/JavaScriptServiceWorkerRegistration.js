d7b7ea1228f953fd046ba2328aff0779768fdf81
Peter Müller <munter@fumle.dk>
2015-11-29 23:15:02 +0100

    test relations/JavaScriptServiceWorkerRegistration href getter/setter

--- test/relations/JavaScriptServiceWorkerRegistration.js
+++ test/relations/JavaScriptServiceWorkerRegistration.js
@@ -14,4 +14,47 @@ describe('relations/JavaScriptServiceWorkerRegistration', function () {
             })
             .run(done);
     });
+
+    it('should read the href correctly', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/JavaScriptServiceWorkerRegistration'})
+            .loadAssets('index.html')
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'JavaScriptServiceWorkerRegistration', 1);
+
+                var relation = assetGraph.findRelations({ type: 'JavaScriptServiceWorkerRegistration' }, true)[0];
+
+                expect(relation, 'to satisfy', {
+                    href: 'sw.js'
+                });
+            })
+            .run(done);
+    });
+
+    it('should write the href correctly', function (done) {
+        new AssetGraph({root: __dirname + '/../../testdata/relations/JavaScriptServiceWorkerRegistration'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relations including unresolved', 'JavaScriptServiceWorkerRegistration', 1);
+
+                var relation = assetGraph.findRelations({ type: 'JavaScriptServiceWorkerRegistration' }, true)[0];
+
+                expect(relation, 'to satisfy', {
+                    href: 'sw.js',
+                    from: {
+                        text: expect.it('not to contain', 'static/serviceworker.js')
+                    }
+                });
+
+                relation.to.url = 'static/serviceworker.js';
+
+                expect(relation, 'to satisfy', {
+                    href: 'static/serviceworker.js',
+                    from: {
+                        text: expect.it('to contain', 'static/serviceworker.js')
+                    }
+                });
+            })
+            .run(done);
+    });
 });
